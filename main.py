7bbb40149703970009d3423de7a5bee3b4591cc5
Guilherme Ferreira Freitas <87777795+glerm-dev@users.noreply.github.com>
2023-09-15 22:00:29 -0300

    "add account history"

--- main.py
+++ main.py
@@ -5,22 +5,39 @@ class BankAccount:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
+        self.extrato = []
 
     def printSaldo(self):
         print(f"Saldo atual: R${self.saldo}")
 
     def deposit(self, depositValue):
+        self.extrato.append("+R$ " + str(depositValue) + " depositado")
+
         self.saldo += depositValue
-        print(f"R${depositValue} foram depositados na conta")
+        print(f"+R${depositValue} foram depositados na conta")
         self.printSaldo() 
 
     def cashout(self, cashoutValue):
+        tax = 0.05
+        
         if(cashoutValue > self.saldo):
             print("Valor de saque execede o valor do saldo")
         elif(cashoutValue <= self.saldo):
-            self.saldo -= cashoutValue
-            print(f"R${cashoutValue} retirados da conta")
-            self.printSaldo()
+            if(cashoutValue > 500):
+                self.extrato.append("-R$ " + str(cashoutValue * tax) + " retirado")
+                self.saldo -= cashoutValue + (cashoutValue * tax)
+                print(f"-R${cashoutValue}, -R${cashoutValue * tax}de taxa retirados da conta")
+                self.printSaldo()
+            else:
+                self.extrato.append("-R$ " + str(cashoutValue) + "retirado")
+                self.saldo -= cashoutValue 
+                print(f"-R${cashoutValue} retirados da conta")
+                self.printSaldo()
+
+    def history(self):
+        print("Historico da conta: ")
+        for i in self.extrato:
+            print(i)
 
 # Funções
 def action():
@@ -46,5 +63,11 @@ account1 = BankAccount(titular_nome, entra_saldo)
 account1.printSaldo()
 
 action()
+
+print("\n")
+
+account1.history()
+
+print("\n")
 
 print("Fechando aplicativo")
