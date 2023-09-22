d4623a71c33f60762021b80c5d3889be07b4929f
Guilherme Ferreira Freitas <87777795+glerm-dev@users.noreply.github.com>
2023-09-21 22:16:02 -0300

    "new file added"

---
+++ bank_account.py
@@ -0,0 +1,37 @@
+class BankAccount:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+        self.extrato = []
+
+    def printSaldo(self):
+        print(f"Saldo atual: R${self.saldo}")
+
+    def deposit(self, depositValue):
+        self.extrato.append("+R$ " + str(depositValue) + " depositado")
+
+        self.saldo += depositValue
+        print(f"+R${depositValue} foram depositados na conta")
+        self.printSaldo() 
+
+    def cashout(self, cashoutValue):
+        tax = 0.05
+        
+        if(cashoutValue > self.saldo):
+            print("Valor de saque execede o valor do saldo")
+        elif(cashoutValue <= self.saldo):
+            if(cashoutValue > 500):
+                self.extrato.append("-R$ " + str(cashoutValue * tax) + " retirado")
+                self.saldo -= cashoutValue + (cashoutValue * tax)
+                print(f"-R${cashoutValue}, -R${cashoutValue * tax}de taxa retirados da conta")
+                self.printSaldo()
+            else:
+                self.extrato.append("-R$ " + str(cashoutValue) + "retirado")
+                self.saldo -= cashoutValue 
+                print(f"-R${cashoutValue} retirados da conta")
+                self.printSaldo()
+
+    def history(self):
+        print("Historico da conta: ")
+        for i in self.extrato:
+            print(i)
--- main.py
+++ main.py
@@ -1,48 +1,10 @@
 # Bank Account;
 
-# Classes
-class BankAccount:
-    def __init__(self, titular, saldo):
-        self.titular = titular
-        self.saldo = saldo
-        self.extrato = []
-
-    def printSaldo(self):
-        print(f"Saldo atual: R${self.saldo}")
-
-    def deposit(self, depositValue):
-        self.extrato.append("+R$ " + str(depositValue) + " depositado")
-
-        self.saldo += depositValue
-        print(f"+R${depositValue} foram depositados na conta")
-        self.printSaldo() 
-
-    def cashout(self, cashoutValue):
-        tax = 0.05
-        
-        if(cashoutValue > self.saldo):
-            print("Valor de saque execede o valor do saldo")
-        elif(cashoutValue <= self.saldo):
-            if(cashoutValue > 500):
-                self.extrato.append("-R$ " + str(cashoutValue * tax) + " retirado")
-                self.saldo -= cashoutValue + (cashoutValue * tax)
-                print(f"-R${cashoutValue}, -R${cashoutValue * tax}de taxa retirados da conta")
-                self.printSaldo()
-            else:
-                self.extrato.append("-R$ " + str(cashoutValue) + "retirado")
-                self.saldo -= cashoutValue 
-                print(f"-R${cashoutValue} retirados da conta")
-                self.printSaldo()
-
-    def history(self):
-        print("Historico da conta: ")
-        for i in self.extrato:
-            print(i)
+from bank_account import *
 
 # Funções
-def action():
-    quit = 0
-    while(quit != 'q'):
+def action(account1):
+    while(True):
         opition = input("d - depositar | s - sacar: ")
         if(opition == 'd'):
             depositValue = float(input("Quantidade a ser depositada: "))
@@ -52,22 +14,24 @@ def action():
             account1.cashout(cashoutValue)
         
         quit = input("Sair(q) | Continuar(n): ")
+        if(quit == 'q'):
+            break
 
 # Main
-print("Entre com os dados da conta: ")
-titular_nome = input("Nome do titular: ")
-entra_saldo = float(input("Saldo na conta: "))
+def main():
 
-account1 = BankAccount(titular_nome, entra_saldo)
+    print("Entre com os dados da conta: ")
+    titular_nome = input("Nome do titular: ")
+    entra_saldo = float(input("Saldo na conta: "))
 
-account1.printSaldo()
+    account1 = BankAccount(titular_nome, entra_saldo)
+    account1.printSaldo()
 
-action()
+    action(account1)
+    
+    print("\n")
+    account1.history()
+    print("\n")
+    print("Fechando aplicativo")
 
-print("\n")
-
-account1.history()
-
-print("\n")
-
-print("Fechando aplicativo")
+main()
